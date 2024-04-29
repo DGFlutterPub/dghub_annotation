aa0e82904173d11b58141f61e55b9f30adc13bc7
thitlwincoder <thitlwincoder@gmail.com>
2024-04-29 15:51:06 +0630

    feat: add models

--- lib/dghub_annotation.dart
+++ lib/dghub_annotation.dart
@@ -1,5 +1,17 @@
-DgHub dghub = const DgHub();
+library dghub_annotation;
 
-class DgHub {
-  const DgHub();
+import 'models/dg_controller.dart';
+import 'models/dg_model.dart';
+import 'models/dg_page.dart';
+import 'models/dg_socket.dart';
+
+DGHubGenerator dgHubGenerator = const DGHubGenerator();
+
+class DGHubGenerator {
+  final List<DGModel>? models;
+  final DGController? controller;
+  final DGSocket? socket;
+  final List<DGPage>? pages;
+
+  const DGHubGenerator({this.models, this.controller, this.socket, this.pages});
 }
